703783a808d501fbfd8b5384db0b32b04b7292e8
Raymond Nsubuga <raymondklyn007@gmail.com>
2025-03-12 21:57:58 +0300

    put sensitive email info in .env file

--- CustomUser-Auth/Helpers/Emailing/EmailService.cs
+++ CustomUser-Auth/Helpers/Emailing/EmailService.cs
@@ -5,10 +5,13 @@ namespace CustomUser_Auth.Helpers.Emailing;
 
 public class EmailService:IEmailService
 {
+    private string MAIL_HOST = Environment.GetEnvironmentVariable("MAIL_HOST");
+    private string MAIL_ADDRESS = Environment.GetEnvironmentVariable("MAIL_ADDRESS");
+    private string MAIL_PASSWORD = Environment.GetEnvironmentVariable("MAIL_PASSWORD");
     public async Task SendEmailAsync(string to, string subject, string body)
     {
         var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("Racoon", "rayrmond.kizito@gmail.com"));
+        message.From.Add(new MailboxAddress("Racoon", MAIL_ADDRESS));
         message.To.Add(new MailboxAddress("Zebra", to));
         message.Subject = subject;
 
@@ -18,8 +21,8 @@ public class EmailService:IEmailService
         };
 
         using var client = new SmtpClient();
-        await client.ConnectAsync("smtp.gmail.com", 465, true);
-        await client.AuthenticateAsync("rayrmond.kizito@gmail.com", "gbhu bckz yoew cxmp");
+        await client.ConnectAsync(MAIL_HOST, 465, true);
+        await client.AuthenticateAsync(MAIL_ADDRESS, MAIL_PASSWORD);
         await client.SendAsync(message);
         await client.DisconnectAsync(true);
     }
